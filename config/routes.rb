85a0905c65bf6753d728c4277f7952aecc339fd1
ahwong5 <ahwong5@gmail.com>
2016-08-30 00:33:23 +0800

    video 2

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  get 'cart/index'
+
+  resources :products
+  root 'page#home'
+
+  get 'page/about'
+
+  get 'page/faqs'
+
+  get 'page/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
